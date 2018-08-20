99121cf9f66036ec610fe12bd896342c3a0f99a1
David Rebbe <drebbe@intrepidcs.com>
2018-08-20 12:58:50 -0400

    updated unit test.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- test/main.cpp
+++ test/main.cpp
@@ -16,6 +16,8 @@ std::string deviceName(Neoradio2DeviceInfo& device)
 	return ss.str();
 }
 
+bool get_device_info(neoradio2_handle& handle, Neoradio2DeviceInfo& device, int bank);
+
 int main(int argc, char* argv[])
 {
 	using namespace std::chrono;
@@ -29,17 +31,8 @@ int main(int argc, char* argv[])
 		ice::Function <void(void)> find_hid_devices(&mgr["libneoradio2"], "find_hid_devices");
 		ice::Function <int(Neoradio2DeviceInfo*, unsigned int*)> neoradio2_find_devices(&mgr["libneoradio2"], "neoradio2_find_devices");
 		ice::Function <int(neoradio2_handle*, Neoradio2DeviceInfo*)> neoradio2_open(&mgr["libneoradio2"], "neoradio2_open");
-		ice::Function <int(neoradio2_handle*, int)> neoradio2_chain_identify(&mgr["libneoradio2"], "neoradio2_chain_identify");
-		ice::Function <int(neoradio2_handle*, int, int*)> neoradio2_chain_is_identified(&mgr["libneoradio2"], "neoradio2_chain_is_identified");
-
-		ice::Function <int(neoradio2_handle*, int, int*)> neoradio2_app_is_started(&mgr["libneoradio2"], "neoradio2_app_is_started");
 		ice::Function <int(neoradio2_handle*, int)> neoradio2_app_start(&mgr["libneoradio2"], "neoradio2_app_start");
-		ice::Function <int(neoradio2_handle*, int, unsigned int*)> neoradio2_get_serial_number(&mgr["libneoradio2"], "neoradio2_get_serial_number");
-		ice::Function <int(neoradio2_handle*, int, int*, int*, int*)> neoradio2_get_manufacturer_date(&mgr["libneoradio2"], "neoradio2_get_manufacturer_date");
-		ice::Function <int(neoradio2_handle*, int, int*, int*)> neoradio2_get_firmware_version(&mgr["libneoradio2"], "neoradio2_get_firmware_version");
-		ice::Function <int(neoradio2_handle*, int, int*, int*)> neoradio2_get_hardware_revision(&mgr["libneoradio2"], "neoradio2_get_hardware_revision");
-		ice::Function <int(neoradio2_handle*, int, int*)> neoradio2_get_device_type(&mgr["libneoradio2"], "neoradio2_get_device_type");
-
+		ice::Function <int(neoradio2_handle*, int)> neoradio2_enter_bootloader(&mgr["libneoradio2"], "neoradio2_enter_bootloader");
 		ice::Function <int(neoradio2_handle*)> neoradio2_close(&mgr["libneoradio2"], "neoradio2_close");
 
 		Neoradio2DeviceInfo devices[8];
@@ -54,6 +47,9 @@ int main(int argc, char* argv[])
 
 		for (auto i=decltype(device_count){0}; i < device_count; ++i)
 		{
+			int bank = 0xFF;
+			int is_identified = 0;
+			int is_started = 0;
 			auto& device = devices[i];
 			neoradio2_handle handle;
 			// Open the device
@@ -63,101 +59,26 @@ int main(int argc, char* argv[])
 				std::cerr << "Failed to open: " << deviceName(device) << "!\n";
 				continue;
 			}
-			// Identify the chain
-			int bank = 0xFF;
-			int is_identified = 0;
-			std::cout << "Identifying Chain on " << deviceName(device) << "\n";
-			if (neoradio2_chain_identify(&handle, bank) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_chain_identify() failed: " << deviceName(device) << "!\n";
-			}
-			if (neoradio2_chain_is_identified(&handle, 0xFF, &is_identified) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_is_chain_identified() failed: " << deviceName(device) << "!\n";
-			}
-			std::cout << "Chain is identified: " << (is_identified ? "True" : "False") << "\n";
-			int is_started = 0;
-			if (neoradio2_app_is_started(&handle, bank, &is_started) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_app_is_started() failed: " << deviceName(device) << "!\n";
-			}
-			std::cout << "Firmware State: " << (is_started ? "Application Level" : "Bootloader Level") << "\n";
 
-			int year, month, day;
-			if (neoradio2_get_manufacturer_date(&handle, bank, &year, &month, &day) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_get_manufacturer_date() failed: " << deviceName(device) << "!\n";
-			}
-			else
-				std::cout << "Manufacturer Date: " << month << "/" << day << "/" << year << "\n";
+			// Always make sure we are in bootloader to start with
+			std::cout << "==============================================================================\n";
+			std::cout << "Entering bootloader...\n";
+			if (neoradio2_enter_bootloader(&handle, bank) != NEORADIO2_SUCCESS)
+				std::cout << "Failed to enter bootloader...\n";
 
-			int major, minor;
-			if (neoradio2_get_firmware_version(&handle, bank, &major, &minor) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_get_firmware_version() failed: " << deviceName(device) << "!\n";
-			}
-			else
-				std::cout << "Firmware Version: " << major << "." << minor << "\n";
+			std::cout << "Getting device information:\n";
+			if (!get_device_info(handle, device, bank))
+				std::cerr << "Failed to get device info...\n";
 
-			if (neoradio2_get_hardware_revision(&handle, bank, &major, &minor) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_get_hardware_revision() failed: " << deviceName(device) << "!\n";
-			}
-			else
-				std::cout << "Hardware Revision: " << major << "." << minor << "\n";
+			std::cout << "==============================================================================\n";
+			std::cout << "Entering Application...\n";
+			if (neoradio2_app_start(&handle, bank) != NEORADIO2_SUCCESS)
+				std::cout << "Failed to start app...\n";
 
-			if (!is_started)
-			{
-				std::cout << "Starting app...\n";
-				if (neoradio2_app_start(&handle, bank) != NEORADIO2_SUCCESS)
-					std::cout << "Failed to start app...\n";
+			std::cout << "Getting device information:\n";
+			if (!get_device_info(handle, device, bank))
+				std::cerr << "Failed to get device info...\n";
 
-				//std::this_thread::sleep_for(2s);
-			}
-
-
-			// APPLICATION LEVEL
-
-			is_identified = 0;
-			std::cout << "Identifying Chain on " << deviceName(device) << "\n";
-			if (neoradio2_chain_identify(&handle, bank) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_chain_identify() failed: " << deviceName(device) << "!\n";
-			}
-			if (neoradio2_chain_is_identified(&handle, 0xFF, &is_identified) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_is_chain_identified() failed: " << deviceName(device) << "!\n";
-			}
-			std::cout << "Chain is identified: " << (is_identified ? "True" : "False") << "\n";
-			is_started = 0;
-			if (neoradio2_app_is_started(&handle, bank, &is_started) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_app_is_started() failed: " << deviceName(device) << "!\n";
-			}
-			std::cout << "Firmware State: " << (is_started ? "Application Level" : "Bootloader Level") << "\n";
-
-			if (neoradio2_get_manufacturer_date(&handle, bank, &year, &month, &day) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_get_manufacturer_date() failed: " << deviceName(device) << "!\n";
-			}
-			else
-				std::cout << "Manufacturer Date: " << month << "/" << day << "/" << year << "\n";
-
-			if (neoradio2_get_firmware_version(&handle, bank, &major, &minor) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_get_firmware_version() failed: " << deviceName(device) << "!\n";
-			}
-			else
-				std::cout << "Firmware Version: " << major << "." << minor << "\n";
-
-			if (neoradio2_get_hardware_revision(&handle, bank, &major, &minor) != NEORADIO2_SUCCESS)
-			{
-				std::cerr << "neoradio2_get_hardware_revision() failed: " << deviceName(device) << "!\n";
-			}
-			else
-				std::cout << "Hardware Revision: " << major << "." << minor << "\n";
-
-			std::this_thread::sleep_for(120s);
 			std::cout << "Closing " << deviceName(device) << "\n";
 			if (neoradio2_close(&handle) != NEORADIO2_SUCCESS)
 			{
@@ -172,4 +93,74 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 	return 0;
+}
+
+
+bool get_device_info(neoradio2_handle& handle, Neoradio2DeviceInfo& device, int bank)
+{
+	try
+	{
+		auto mgr = ice::LibraryManager::getLibraryManager();
+		mgr.add("libneoradio2", "libneoradio2.dll");
+		//std::cout << mgr["libneoradio2"].getPath() << "\n";
+		ice::Function <int(neoradio2_handle*, int)> neoradio2_chain_identify(&mgr["libneoradio2"], "neoradio2_chain_identify");
+		ice::Function <int(neoradio2_handle*, int, int*)> neoradio2_chain_is_identified(&mgr["libneoradio2"], "neoradio2_chain_is_identified");
+		ice::Function <int(neoradio2_handle*, int, int*)> neoradio2_app_is_started(&mgr["libneoradio2"], "neoradio2_app_is_started");
+
+		ice::Function <int(neoradio2_handle*, int, unsigned int*)> neoradio2_get_serial_number(&mgr["libneoradio2"], "neoradio2_get_serial_number");
+		ice::Function <int(neoradio2_handle*, int, int*, int*, int*)> neoradio2_get_manufacturer_date(&mgr["libneoradio2"], "neoradio2_get_manufacturer_date");
+		ice::Function <int(neoradio2_handle*, int, int*, int*)> neoradio2_get_firmware_version(&mgr["libneoradio2"], "neoradio2_get_firmware_version");
+		ice::Function <int(neoradio2_handle*, int, int*, int*)> neoradio2_get_hardware_revision(&mgr["libneoradio2"], "neoradio2_get_hardware_revision");
+		ice::Function <int(neoradio2_handle*, int, int*)> neoradio2_get_device_type(&mgr["libneoradio2"], "neoradio2_get_device_type");
+
+
+		int is_identified = 0;
+		int is_started = 0;
+		int major, minor;
+		int year, month, day;
+		std::cout << "Identifying Chain on " << deviceName(device) << "\n";
+		if (neoradio2_chain_identify(&handle, bank) != NEORADIO2_SUCCESS)
+		{
+			std::cerr << "neoradio2_chain_identify() failed: " << deviceName(device) << "!\n";
+		}
+		if (neoradio2_chain_is_identified(&handle, 0xFF, &is_identified) != NEORADIO2_SUCCESS)
+		{
+			std::cerr << "neoradio2_is_chain_identified() failed: " << deviceName(device) << "!\n";
+		}
+		std::cout << "Chain is identified: " << (is_identified ? "True" : "False") << "\n";
+
+		if (neoradio2_app_is_started(&handle, bank, &is_started) != NEORADIO2_SUCCESS)
+		{
+			std::cerr << "neoradio2_app_is_started() failed: " << deviceName(device) << "!\n";
+		}
+		std::cout << "Firmware State: " << (is_started ? "Application Level" : "Bootloader Level") << "\n";
+		if (neoradio2_get_manufacturer_date(&handle, bank, &year, &month, &day) != NEORADIO2_SUCCESS)
+		{
+			std::cerr << "neoradio2_get_manufacturer_date() failed: " << deviceName(device) << "!\n";
+		}
+		else
+			std::cout << "Manufacturer Date: " << month << "/" << day << "/" << year << "\n";
+
+		if (neoradio2_get_firmware_version(&handle, bank, &major, &minor) != NEORADIO2_SUCCESS)
+		{
+			std::cerr << "neoradio2_get_firmware_version() failed: " << deviceName(device) << "!\n";
+		}
+		else
+			std::cout << "Firmware Version: " << major << "." << minor << "\n";
+
+		if (neoradio2_get_hardware_revision(&handle, bank, &major, &minor) != NEORADIO2_SUCCESS)
+		{
+			std::cerr << "neoradio2_get_hardware_revision() failed: " << deviceName(device) << "!\n";
+		}
+		else
+			std::cout << "Hardware Revision: " << major << "." << minor << "\n";
+
+		return true;
+	}
+	catch (ice::Exception& ex)
+	{
+		std::cerr << "EXCEPTION: " << ex.whatString() << "\n";
+		return false;
+	}
+	return false;
 }
