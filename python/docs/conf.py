9372f88fad31e7d4df0297aaba2df7a396e71e99
David Rebbe <drebbe@intrepidcs.com>
2018-10-09 17:52:17 -0400

    More documentation changes.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- python/docs/conf.py
+++ python/docs/conf.py
@@ -206,7 +206,7 @@ html_static_path = ['_static']
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'python_exampledoc'
+htmlhelp_basename = 'neoradio2doc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -228,7 +228,7 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'python_example.tex', u'python_example Documentation',
+    (master_doc, 'neoradio2.tex', u'neoRAD-IO2 Documentation',
      u'Sylvain Corlay', 'manual'),
 ]
 
@@ -258,7 +258,7 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'python_example', u'python_example Documentation',
+    (master_doc, 'neoradio2', u'neoRAD-IO2 Documentation',
      [author], 1)
 ]
 
@@ -272,8 +272,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'python_example', u'python_example Documentation',
-     author, 'python_example', 'One line description of project.',
+    (master_doc, 'neoradio2', u'neoRAD-IO2 Documentation',
+     author, 'neoradio2', u'neoRAD-IO2 Documentation',
      'Miscellaneous'),
 ]
 
