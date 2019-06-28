7fd80a9b670166ba4e37357c38405b95d4227b4d
David Rebbe <drebbe@intrepidcs.com>
2019-06-28 01:18:45 -0400

    Updated script to work on linux.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- generate_swig_bindings.sh
+++ generate_swig_bindings.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-swig_executable="/c/swig/swigwin-4.0.0/swig.exe"
+if [ "$OSTYPE" = "win32" ]; then
+    swig_executable="/c/swig/swigwin-4.0.0/swig.exe"
+else
+    swig_executable="swig"
+fi
+
 supported_langs=("csharp" "d" "go" "guile" "java" "javascript" "lua" "octave" "perl5" "php7" "r" "ruby" "tcl8" "xml")
 
 for language in "${supported_langs[@]}"; do
@@ -13,7 +18,11 @@ for language in "${supported_langs[@]}"; do
     mkdir -pv "./swig/${language}"
     echo "Generating ${language} Bindings..."
     
+    cd "./swig/${language}"
+    
     
     #echo "${swig_executable} -outdir ./swig/${language} -${language} swig.i"
-    $swig_executable -outdir ./swig/$language -$language $extra_opts swig.i
+    $swig_executable -$language $extra_opts ../../swig.i
+    
+    cd ../..
 done
