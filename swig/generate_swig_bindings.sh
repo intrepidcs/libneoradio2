92fbc0875cc38e6e169d15d22dd40209ea0ddf6a
David Rebbe <drebbe@intrepidcs.com>
2019-07-09 12:01:35 -0400

    Added new swig layout, still a WIP.
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- swig/generate_swig_bindings.sh
+++ swig/generate_swig_bindings.sh
@@ -6,6 +6,8 @@ else
     swig_executable="swig"
 fi
 
+command -v $swig_executable >/dev/null 2>&1 || { echo >&2 "I require ${swig_executable} but it's not installed.  Aborting."; exit 1; }
+
 supported_langs=("csharp" "d" "go" "guile" "java" "javascript" "lua" "octave" "perl5" "php7" "r" "ruby" "tcl8" "xml")
 #supported_langs=("csharp")
 
@@ -16,10 +18,11 @@ for language in "${supported_langs[@]}"; do
     elif [ "$language" = "javascript" ]; then
         extra_opts=" -node"
     fi
-    mkdir -pv "./swig/${language}"
+    mkdir -pv "./${language}/${language}"
     echo "Generating ${language} Bindings..."
     
-    cd "./${language}"
-    $swig_executable -$language $extra_opts -outcurrentdir ../swig.i
-    cd ../
+    cd "./${language}/${language}"
+    rm *
+    $swig_executable -c++ -$language $extra_opts -outcurrentdir ../../swig.i
+    cd ../../
 done
