4a61c8961a137bb6f33a42e32f73d2844147e20a
David Rebbe <drebbe@intrepidcs.com>
2019-07-16 14:41:48 -0400

    Fixed Python build issues with OS X. Fixes Issue #4.
    -Tested on macOS Heigh Sierra 10.13.6.
    
    Signed-off-by: David Rebbe <drebbe@intrepidcs.com>

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,7 @@ import sys
 import setuptools
 from distutils.sysconfig import get_python_inc
 import os
+import platform
 import version
 
 __version__ = version._get_version_str()
@@ -24,16 +25,23 @@ class get_pybind_include(object):
         return pybind11.get_include(self.user)
 
 
-source_includes = ['src/main.cpp', '../fifo.c', '../device.cpp', 
+source_includes = ['src/main.cpp', '../fifo.c', '../device.cpp',
     '../hiddevice.cpp', '../libneoradio2.cpp', '../neoradio2device.cpp',]
 
 library_includes = []
 if 'NT' in os.name.upper():
     source_includes.append('../hidapi/windows/hid.c')
+elif platform.system().upper() == 'DARWIN':
+    source_includes.append('../hidapi/mac/hid.c')
+    #library_includes.append('IOKit')
 else:
     source_includes.append('../hidapi/linux/hid.c')
     library_includes.append('udev')
 
+# Clang complains about the c++14 flag when its the clang c compiler so lets force
+# it here to use the clang++ compiler always.
+if platform.system().upper() == 'DARWIN':
+    os.environ["LDFLAGS"] = "-framework IOKit -framework CoreFoundation"
 
 ext_modules = [
     Extension(
@@ -72,35 +80,60 @@ def has_flag(compiler, flagname):
             return False
     return True
 
-
 def cpp_flag(compiler):
-    """Return the -std=c++[11/14] compiler flag.
-
-    The c++14 is prefered over c++11 (when it is available).
+    """Return the -std=c++[11/14/17] compiler flag.
+    The newer version is prefered over c++11 (when it is available).
     """
-    if has_flag(compiler, '-std=c++14'):
+
+    if platform.system().upper() == 'DARWIN':
         return '-std=c++14'
-    elif has_flag(compiler, '-std=c++11'):
-        return '-std=c++11'
-    else:
-        raise RuntimeError('Unsupported compiler -- at least C++11 support '
-                           'is needed!')
+
+    flags = [ '-std=c++17', '-std=c++14', '-std=c++11']
+
+    for flag in flags:
+        if has_flag(compiler, flag):
+            return flag
+
+    raise RuntimeError('Unsupported compiler -- at least C++11 support '
+                       'is needed!')
 
 
 class BuildExt(build_ext):
     """A custom build extension for adding compiler-specific options."""
-        
     c_opts = {
         'msvc': ['/EHsc', '/TP', '/D_CRT_SECURE_NO_WARNINGS',],
         'unix': ['-Wno-unused-function'],
     }
+    l_opts = {
+        'msvc': [],
+        'unix': [],
+    }
 
-    if sys.platform == 'darwin':
-        c_opts['unix'] += ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+    if platform.system().upper() == 'DARWIN':
+        darwin_opts = ['-stdlib=libc++', '-mmacosx-version-min=10.7']
+        darwin_l_opts = []
+        c_opts['unix'] += darwin_opts
+        l_opts['unix'] += darwin_l_opts + darwin_opts
 
     def build_extensions(self):
         ct = self.compiler.compiler_type
+        if platform.system().upper() == 'DARWIN':
+            # We need to override the UnixCCompiler._compile method and strip out
+            # clang++ compiler flags since distutils assumes flags are for both
+            # c and c++ compilers.
+            old_compile_func = self.compiler._compile
+            def _new_compile(self, obj, src, ext, cc_args, extra_postargs):
+                #print("COMPILING {}...".format(src))
+                if src.endswith('.c'):
+                    print("WE ARE A C SOURCE FILE:", src, '\n\next:', ext, '\n\ncc_args:', cc_args, '\n\nextra_postargs:', extra_postargs)
+                    cc_args = [ x for x in cc_args if not '-std=' in x]
+                #print("CC_ARGS:", cc_args, '\n')
+                return old_compile_func(self, obj, src, ext, cc_args, extra_postargs)
+            self.compiler._compile = _new_compile
+
+        #print("COMPILER TYPE:", self.compiler, ct)
         opts = self.c_opts.get(ct, [])
+        link_opts = self.l_opts.get(ct, [])
         if ct == 'unix':
             opts.append('-DVERSION_INFO="%s"' % self.distribution.get_version())
             opts.append(cpp_flag(self.compiler))
@@ -110,7 +143,10 @@ class BuildExt(build_ext):
             opts.append('/DVERSION_INFO=\\"%s\\"' % self.distribution.get_version())
         for ext in self.extensions:
             ext.extra_compile_args = opts
+            ext.extra_link_args = link_opts
         build_ext.build_extensions(self)
+
+
 
 setup(
     name='neoradio2',
@@ -122,6 +158,7 @@ setup(
     long_description='',
     ext_modules=ext_modules,
     install_requires=['pybind11>=2.2'],
+    setup_requires=['pybind11>=2.2'],
     cmdclass={'build_ext': BuildExt},
     zip_safe=False,
 )
